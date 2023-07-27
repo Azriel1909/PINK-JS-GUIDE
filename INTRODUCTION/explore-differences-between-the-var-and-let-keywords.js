c51f682d455be4268ddeec713926ac0826de3d27
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-07-26 22:07:34 -0600

    first commit

---
+++ INTRODUCTION/explore-differences-between-the-var-and-let-keywords.js
@@ -0,0 +1,15 @@
+// Var Variables: You can easy overwrite declarations
+
+/*
+var camper = 'Ximena';
+var camper = 'Andreas';
+console.log(camper)
+*/
+
+// Keyword Let was introduced in ES6, a major update to Js
+
+let catName = 'Oliver';
+let catSound = 'Meow!';
+console.log(catName)
+
+

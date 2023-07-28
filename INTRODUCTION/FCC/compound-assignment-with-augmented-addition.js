2b2b18c7a6809fa7e8337b078651dff7cb457868
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-07-28 17:07:21 -0600

    Strings with double quotes

---
+++ INTRODUCTION/FCC/compound-assignment-with-augmented-addition.js
@@ -0,0 +1,19 @@
+// ? Everything to the right is evaluated 1st
+let a = 3;
+let b = 17;
+let c = 12;
+
+// ! Operator which do both a mathematical operation and assignment in one step
+
+// let myVar = 1;
+// myVar += 9;
+// console.log(myVar);
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+console.log(a);
+console.log(b);
+console.log(c);
+

c5eb33762ab1521cee08dfaa382075bdcfc5f064
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-07-27 22:12:33 -0600

    Data Types

---
+++ INTRODUCTION/p2.js
@@ -0,0 +1,20 @@
+// Number - All types of numbers
+let age = 40;
+
+// String - We usually call it text
+let myName = 'Ximena';
+const myLastName = 'Toledo';
+let myChar = 'X';
+
+// Boolean - We have two values (True or false)
+let isFriday = true;
+let isNineOClock = false;
+
+// Undefined - Is a variable that has not been assigned a value
+// Null - is an unintentional empty value
+
+let myNumber = 9;
+let myString = 'I love pizza';
+const myBoolean = true;
+let myUndefined;
+let myNull = null;

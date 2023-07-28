f660399b3661d8767e276fed02e5420e6f44985a
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-07-27 21:54:55 -0600

    Variables

---
+++ INTRODUCTION/p1.js
@@ -0,0 +1,15 @@
+// Write your code below this line
+// Variables are containers for data
+let someNumber = 9;
+const name = 'Ximena Toledo';
+var lightIsOn = false;
+
+/**Formula
+ * let/const/var variableName = variableValue
+ */
+
+let myNumber = 15;
+console.log(myNumber)
+
+const PI = 3.1415;
+console.log(PI);

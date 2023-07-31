a23b748c9bd7b8b2d78d816c3c295d45c59ee60d
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-07-30 20:49:34 -0600

    Strings Concatenation

---
+++ INTRODUCTION/FCC/concatenating-strings-with-plus-operator.js
@@ -0,0 +1,6 @@
+// In javaScript, we use the '+' Concatenation Operator to concatenate Strings.
+const ourStr = "I come first. " + "I come second.";
+console.log(ourStr);
+
+const myStr = "This is the start." + ' This is the end.';
+console.log(myStr);
---
+++ INTRODUCTION/FCC/concatenating-strings-with-the-plus-equals-operator.js
@@ -0,0 +1,9 @@
+/** We can use the += operator to concatenate a string onto the end of an existing string variable. This can be helpful to break a long string over several lines */
+
+let ourStr = "I come first. ";
+ourStr += "I come second.";
+console.log(ourStr);
+
+let myStr = 'This is the first sentence.';
+myStr += ' This is the second sentence.'
+console.log(myStr);

6a3d3720dd0c66b659e7b9484d99fe7531de5e71
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-07-30 20:42:38 -0600

    Strings

---
+++ INTRODUCTION/FCC/escape-sequences-in-strings.js
@@ -0,0 +1,14 @@
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr)
+/**
+ * Code   Output
+ * \'     single quote
+ * \"     double quote
+ * \\     backslash
+ * \n     newline
+ * \t     tab
+ * \r     carriage return
+ * \b     backspace
+ * \f     form feed
+ */
+
---
+++ INTRODUCTION/FCC/quoting-strings-with-single-quotes.js
@@ -0,0 +1,14 @@
+// const doubleQuoteStr = "This is a string"; 
+// const singleQuoteStr = 'This is also a string';
+// const conversation = 'Finn exclaims to Jake, "Algebraic!"';
+// const goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"'; 
+// ! In the goodStr above, you can use both quotes safely by using the backslash \ as an escape character.
+
+// console.log(doubleQuoteStr)
+// console.log(singleQuoteStr)
+// console.log(conversation)
+// console.log(goodStr)
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+console.log(myStr)

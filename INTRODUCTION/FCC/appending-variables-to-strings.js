54bf9d02c61d76beadc082a4a9bc8831715bcbf4
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-07-30 20:54:52 -0600

    Appending

---
+++ INTRODUCTION/FCC/appending-variables-to-strings.js
@@ -0,0 +1,11 @@
+// We can append variables to a string using the plus equals += operator
+
+const anAdjective = "awesome!";
+let ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+console.log(ourStr)
+
+const someAdjective = "awesome!";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+console.log(myStr)
---
+++ INTRODUCTION/FCC/constructing-strings-with-variables.js
@@ -0,0 +1,8 @@
+const ourName = "freeCodeCamp";
+const ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+console.log(ourStr);
+
+const myName = "Ximena Toledo";
+const myStr = "Hello, my name is " + myName + ', What\'s up?';
+console.log(myStr);

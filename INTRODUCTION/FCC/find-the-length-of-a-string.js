45ffd4ae6ef072865cb5a5e25fc98e2e21d3d468
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-07-30 21:06:51 -0600

    Indexing

---
+++ INTRODUCTION/FCC/find-the-length-of-a-string.js
@@ -0,0 +1,9 @@
+// We can find the lenght of a String value by Writing .length
+
+console.log("XimenaToledo".length);
+console.log("Ximena Toledo".length);
+
+let lastNameLength = 0; 
+const lastName = "Lovelace";
+lastNameLength = lastName.length;
+console.log(lastName.length);
---
+++ INTRODUCTION/FCC/use-bracket-notation-to-find-the-first-character-in-a-string.js
@@ -0,0 +1,6 @@
+// Bracket Notation is a way to get a character at a specific index within a string. JavaScript is a Zero-based indexing languages
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0]; 

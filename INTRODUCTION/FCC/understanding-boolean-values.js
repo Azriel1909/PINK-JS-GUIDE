39d00596d6f553afe7b1eff64552bd5374f176e1
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-08-03 20:51:32 -0600

    Functions: Conditionals

---
+++ INTRODUCTION/FCC/understanding-boolean-values.js
@@ -0,0 +1,7 @@
+// Boolean Value (another data type), booleand may only be True or False
+// They're basically little on-off switches 
+// These Two states are mutually exclusive
+
+function welcomeToBooleans() {
+  return true;
+}
---
+++ INTRODUCTION/FCC/use-conditional-logic-with-if-statements.js
@@ -0,0 +1,15 @@
+// Conditionals
+
+/** If statements are used to make decisions in code.*/
+
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+      return 'Yes, that was true';
+    }
+    return 'No, that was false';
+}
+
+console.log(trueOrFalse(true));
+console.log(trueOrFalse(false));
+
+

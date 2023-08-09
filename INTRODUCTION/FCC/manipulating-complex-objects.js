aa04a1cb8038cd397b1f95b3a4833ab0b9e5771a
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-08-08 18:03:12 -0600

    Manipulating Complex Objects

---
+++ INTRODUCTION/FCC/manipulating-complex-objects.js
@@ -0,0 +1,26 @@
+const myMusic = [
+  {
+    // Key    Value
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    // Key    Value
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];

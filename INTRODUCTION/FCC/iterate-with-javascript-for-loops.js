864a9c72e04af5737ddb2bf4fbb0f9d8b167c8a8
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-08-09 20:20:29 -0600

    Loops

---
+++ INTRODUCTION/FCC/iterate-with-javascript-for-loops.js
@@ -0,0 +1,11 @@
+// For loops
+const myArray = [];
+/**
+for (a; b; c), where a is the initialization statement, b is the
+condition statement, and c is the final expression.
+ */
+for (let i = 1; i < 6; i++) {
+  myArray.push(i);
+}
+
+console.log(myArray);
---
+++ INTRODUCTION/FCC/iterate-with-javascript-while-loops.js
@@ -0,0 +1,20 @@
+// Iterate - While Loops
+// It runs while a specificed condition is true and stops once that condition is no longer true
+
+const myArray = [];
+let i = 5;
+
+/*
+while (i < 5) {
+  console.log(myArray.push(i));
+  i++;
+}
+*/
+
+while (i > -1) {
+  myArray.push(i);
+  i--;
+}
+
+console.log(myArray);
+

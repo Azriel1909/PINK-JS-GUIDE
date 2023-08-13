eb0e9450e38a493f31f0d4292512be798b4c220c
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-08-12 23:32:16 -0600

    Default Parameters

---
+++ ES6/set-default-parameters-for-your-functions.js
@@ -0,0 +1,7 @@
+/**
+ * Default Parameters
+ * The default parameter Kicks in when the argument is not
+ * argument is not specified (it is undefined).
+ * 
+ */
+const increment = (number, value = 1) => number + value;

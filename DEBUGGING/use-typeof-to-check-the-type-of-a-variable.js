933c6e97126b8bdca2fd6a9272c5cb4cf58a9689
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-08-22 21:43:06 -0600

    Catching errors

---
+++ DEBUGGING/use-typeof-to-check-the-type-of-a-variable.js
@@ -0,0 +1,20 @@
+
+/**
+ * Typeof
+ * We can check the data structure, or type of a variable
+ * console.log(typeof "");
+console.log(typeof 0);
+console.log(typeof []);
+console.log(typeof {});
+
+ */
+
+
+let seven = 7;
+let three = "3";
+console.log(typeof seven);
+console.log(typeof three);
+
+
+
+

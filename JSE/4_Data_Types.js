659d20476b214d318105faa660d69cffa2d26107
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-13 20:50:54 -0600

    JSE: Number and Special Values

--- JSE/4_Data_Types.js
+++ JSE/4_Data_Types.js
@@ -35,7 +35,55 @@ let isStringTooLong = false;
 let isGameOver = false;
 continueLoop = true;
    
-console.log(false);               // -> false
-console.log(typeof(false));        // -> boolean
-console.log(isDataValid);         // -> true
-console.log(typeof(isDataValid));  // -> boolean
+console.log(false);                 // -> false
+console.log(typeof(false));         // -> boolean
+console.log(isDataValid);           // -> true
+console.log(typeof(isDataValid));   // -> boolean
+
+// + Number: It represents both real numbers and integers.
+// ! The integers values should be limited in JavaScript to the range -(253-1) to (253-1)
+
+console.log("--> Number");
+const randomYear = 1999;
+let delayInSeconds = 0.00019;
+let myArea = (16 * 3.14);
+let halfArea = myArea/2;
+let typeOfRandomYear = typeof(randomYear);
+
+console.log(randomYear);                // -> 1999;
+console.log(typeof(randomYear));        // -> number;
+console.log(typeof(typeOfRandomYear));  // -> string
+
+// + Number: We can represent hexadecimal numbers (0x...), octal (0o...) or binary (0b...).
+
+let a = 10;     // decimal (default)
+let b = 0x10;   // hexadecimal
+let c = 0o10;   // octal
+let d = 0b10;   // binary
+   
+console.log(a); // -> 10
+console.log(b); // -> 16
+console.log(c); // -> 8
+console.log(d); // -> 2
+   
+let x = 9e9;
+let y = 123e-4;
+console.log(x);
+console.log(y); 
+
+console.log("--> Special values");
+
+// + Number: We can use special values which are Infinity, -Infinity and NaN (not a number).
+
+let e = 1 / 0;
+let f = -Infinity;
+   
+console.log(e);          // -> Infinity
+console.log(f);          // -> -Infinity
+console.log(typeof(e));  // -> number
+console.log(typeof(f));  // -> number
+   
+let g = "it's definitely not a number";
+let h = g * 10;
+console.log(h);          // -> NaN
+console.log(typeof(h));  // -> number

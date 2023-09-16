6725863f049f7c76f1b2178b879d72a6047907c4
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-15 21:46:59 -0600

    JSE: Modifying Arrays

---
+++ JSE/8_Arrays.js
@@ -0,0 +1,25 @@
+// ARRAYS
+
+// + An array is a complex data type that can be used to store a data collection (arrays only store values, without the associated names or keys).
+
+// ? We use Bracket Notation
+
+let days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+
+console.log(days[0])
+console.log(days[1])
+console.log(days[2])
+console.log(days[3])
+console.log(days[4])
+console.log(days[5])
+console.log(days[6])
+
+console.log('----- Modifying Arrays');
+console.log(days)
+days[0] = 'Mon';
+console.log(days)
+
+
+
+
+

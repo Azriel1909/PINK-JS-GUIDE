2720353c8ef09ab6b69c5d4cbd987d005b55edb1
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-16 12:53:20 -0600

    JSE: Train Ticket

---
+++ JSE/9_Objects_Ticket.js
@@ -0,0 +1,9 @@
+let trainTicket = {
+  from: 'Berlin',
+  to: 'Platz',
+  price: 10
+}
+
+console.log(trainTicket)
+console.log(`Ticket from ${trainTicket.from}, to ${trainTicket.to}, price ${trainTicket.price}`)
+console.log(`Data Type: ${typeof(trainTicket)}`)

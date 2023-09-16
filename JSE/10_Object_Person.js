2749f81ab40ee3414f9e914767b21e88c5f1f91e
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-16 12:56:30 -0600

    JSE: A person

---
+++ JSE/10_Object_Person.js
@@ -0,0 +1,7 @@
+let aPerson = {
+  name: 'Ximena',
+  surname: 'Xime',
+  age: 40
+}
+
+console.log(`Hi! my name is ${aPerson.name}, my surname is ${aPerson.surname} and I am ${aPerson.age} years old.`)

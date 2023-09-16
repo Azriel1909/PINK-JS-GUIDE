1f7f052d9f336366bad92a0e6c0d83aa8572beaf
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-16 13:29:25 -0600

    JSE: Contact List

---
+++ JSE/12_Lab_Data_Types.js
@@ -0,0 +1,30 @@
+let contacts = [
+  {
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    number: "Curabitur.egestas.nunc@nonummyac.co.uk" 
+  },
+  {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    number: "posuere.vulputate@sed.com"
+  },
+  {
+    name: "Helen Richards",
+    phone: "0800 1111",
+    number: "libero@convallis.edu"
+  }
+]
+
+console.log(contacts.length)
+
+contacts.push({
+  name: "Maisie Haley",
+  phone: "0913 531 3030",
+  number: "risus.Quisque@urna.ca"
+})
+
+console.log(contacts)
+console.log(contacts.length)
+console.log(contacts[0])
+console.log(contacts[3])

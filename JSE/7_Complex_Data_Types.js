ce1643ca8d7b88a9d3d6079e3fd347f800a805f5
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-15 21:33:06 -0600

    JSE: Objects

---
+++ JSE/7_Complex_Data_Types.js
@@ -0,0 +1,33 @@
+// COMPLEX DATA TYPES
+
+// + Objects
+
+// * Record: Is a collection of named fields. Each field has its own name (or key) and value assigned to it.
+
+let myObject = {
+  // ? Properties (Key values pairs)
+  id: 12345,
+  str: 'Ximena Toledo'
+};
+
+console.log('----------- Object');
+console.log(myObject);
+
+// * A specific property of an object can be referred to with Dot Notation.
+
+console.log('----------- Dot Notation');
+console.log(myObject.id);
+console.log(myObject.str);
+
+let user1 = {
+  name: 'Ximena Toledo',
+  surname: 'Pinki',
+  age: 30,
+  email: 'pinki@gmail.com'
+}
+
+console.log('----------- User Object');
+console.log(user1)
+
+
+

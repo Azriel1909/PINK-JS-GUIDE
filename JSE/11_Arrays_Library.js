1d0c9d2961b70485f748f03267742f8b864d9aee
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-16 13:02:25 -0600

    JSE: A Library

---
+++ JSE/11_Arrays_Library.js
@@ -0,0 +1,19 @@
+let myLibrary = [
+  {
+    title: 'Speaking JavaScript',
+    author: 'Axel Rauschmayer',
+    pages: 460
+  },
+  {
+    title: 'Programming JavaScript Applications',
+    author: 'Eric Elliott',
+    pages: 254
+  },
+  {
+    title: 'Understanding EMACScript 6',
+    author: 'Nicholas C. Zakas',
+    pages: 352
+  }
+]
+
+console.log(myLibrary)

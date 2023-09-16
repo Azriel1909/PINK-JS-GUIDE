89189bd7dab09a13c67e0d1289a82db3115f333d
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-16 13:19:14 -0600

    JSE: A Library

--- JSE/11_Arrays_Library.js
+++ JSE/11_Arrays_Library.js
@@ -17,3 +17,35 @@ let myLibrary = [
 ]
 
 console.log(myLibrary)
+
+myLibrary.push({
+  title: 'Learning JavaScript Design Patterns',
+  author: 'Addy Osmani',
+  pages: 254
+})
+
+console.log('---------------------')
+console.log(myLibrary)
+
+console.log('---------------------')
+let slicedBooks = myLibrary.slice(-2)
+console.log(slicedBooks)
+
+console.log('---------------------')
+let deletedBook = myLibrary.shift()
+console.log(deletedBook)
+console.log(myLibrary)
+
+console.log('---------------------')
+
+book1 = myLibrary[0].pages;
+book2 = myLibrary[1].pages;
+book3 = myLibrary[2].pages;
+
+console.log(`Data Types: ${typeof(book1)}, ${typeof(book2)}, ${typeof(book3)}`)
+
+let totalPages = book1 + book2 + book3;
+console.log(`Total Pages = ${totalPages}`)
+
+// let sum = myLibrary[0].pages + myLibrary[1].pages + myLibrary[2].pages; console.log(`pages: ${sum}`);
+// console.log(sum)

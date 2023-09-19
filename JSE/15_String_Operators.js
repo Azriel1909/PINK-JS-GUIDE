1a7708922b80f682dd457b7acb5bf827577af10d
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-18 21:03:38 -0600

    JSE: Compound Assignment

--- JSE/15_String_Operators.js
+++ JSE/15_String_Operators.js
@@ -2,7 +2,7 @@
 
 // + Concatenation Operators: This operator will convert everything to a String if any of the operands is a String type. Finally, it will create a new character string, attaching the right operand at the end of the left operand.
 
-console.log('-------------------')
+console.log('------------------- Concatenation Operators')
 let myGreeting = 'Hi! Good Morning'
 let randomName = 'Ximena'
 console.log(myGreeting + ' ' + `${randomName}`)
@@ -10,3 +10,12 @@ console.log(myGreeting + ' ' + `${randomName}`)
 let sayGoodbye = 'Goodbye!'
 let mySentence = 'It is time to say ' + sayGoodbye
 console.log(`${mySentence}, Data Type: ${typeof(mySentence)}`)
+
+// + Compound Assignment Operators
+
+console.log('------------------- Compound Assignment Operators')
+
+let myNewSentence = 'Happy birthday...'
+myNewSentence += ' David '
+myNewSentence += 2929
+console.log(myNewSentence)

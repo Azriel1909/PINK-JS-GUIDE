1001b45d070b5c02b2aeda9e807c5a14d1dc1d67
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-20 09:09:44 -0600

    JSE: User volume

---
+++ JSE/18_User_Volume.js
@@ -0,0 +1,5 @@
+let userWidth = Number(window.prompt('Enter your Width: '))
+let userHeight = Number(window.prompt('Enter your Height: '))
+let userLength = Number(window.prompt('Enter your Length: '))
+let volume = userWidth * userHeight * userLength
+alert('Volume: ' + volume)

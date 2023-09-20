8b22fea6e661af336ff0e54f6c5a90cffc673f99
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-20 09:03:11 -0600

    JSE: Prompt Dialog Box

--- JSE/17_User_Interaction.js
+++ JSE/17_User_Interaction.js
@@ -2,7 +2,7 @@
 
 // * DOM (Document Object Model)
 
-// + Dialog Boxes: Are popup windows (modal windows) which mean that when the dialog box is displayed, it is not possible to interact with the webpage itself until this dialog box is closed.
+// > Dialog Boxes: Are popup windows (modal windows) which mean that when the dialog box is displayed, it is not possible to interact with the webpage itself until this dialog box is closed.
 
 // + Alert Dialog Box: We need to call a method name alert(). This method accept one optional parameter (the text that will be displayed).
 
@@ -13,3 +13,23 @@ window.alert('Hello, World! Again!')
 alert(4 * 7)
 alert(false)
 alert('Some milk', 'Some sugar')
+
+// + Confirm Dialog Box: Similar to the alert method, the difference between alert and confirm is that confirm dialog box displays to buttons, the OK button and the Cancel button. Depending on button pressed by the user, the confirm method returns a Boolean value. OK -> true and Cancel -> false
+
+let myDecision = window.confirm('It that OK?')
+console.log(myDecision)
+
+let removeQuestion = confirm('Remove all data?')
+let myMessage = removeQuestion ? 'Deleting Data...' : 'Cancelled'
+console.log(myMessage)
+
+// + Prompt Dialog Box: like a confirm dialog box (it contains the OK and Cancel buttons), but also contains a single-line text field that allows the user to input text.
+
+// * As with other dialog boxes, the prompt accepts an optional parameter as a message that will be displayed. The prompt also accepts a second optional parameter, which is the default value of the text field visible in the dialog window. 
+
+// * If the user closes the window with the OK button, anything in the text field will be returned from the prompt method as a String, if the dialog box is closed with the Cancel button, the method will return a null value.
+
+let myFood = window.prompt('What is your favorite food? ', 'pizza')
+myFood = myFood ? myFood : 'Anonymous'
+let myDrink = prompt('My favorite food is ' + myFood +', What is your favorite drink? ')
+alert('My favorite drink is ' + myDrink + ' of course!')

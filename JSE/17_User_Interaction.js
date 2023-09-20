36e87b124b53a26fb8fa0ac3d4ee27d44a925434
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-20 08:49:24 -0600

    JSE: Dialog Box

---
+++ JSE/17_User_Interaction.js
@@ -0,0 +1,15 @@
+// * The user influences not only the data, but also its execution. THe user gives a path data to the program being run in this way in the form of a configuration file and/or as call arguments.
+
+// * DOM (Document Object Model)
+
+// + Dialog Boxes: Are popup windows (modal windows) which mean that when the dialog box is displayed, it is not possible to interact with the webpage itself until this dialog box is closed.
+
+// + Alert Dialog Box: We need to call a method name alert(). This method accept one optional parameter (the text that will be displayed).
+
+// ? The alert method is method of the object window, ti can be used both, alert() or window.alert. 
+
+alert('Hi, my name is Ximena!')
+window.alert('Hello, World! Again!')
+alert(4 * 7)
+alert(false)
+alert('Some milk', 'Some sugar')

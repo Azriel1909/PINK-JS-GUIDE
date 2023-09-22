c711d476622ef415b3a91d8f0ef1e87e95e9208e
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-21 21:42:11 -0600

    JSE: If statement

---
+++ JSE/19_Conditionals.js
@@ -0,0 +1,44 @@
+// > Conditional Statements or Flow Control Statements
+
+// * The if Statement
+
+let userAnswer = confirm('Are you tired?')
+console.log(userAnswer)
+
+if (userAnswer) {
+  alert('The user us tired!')
+}
+
+// it returns true if the user click on the Ok button
+
+let unitPrice = 19
+let somePieces = prompt('How many pieces do you want to order?', 0)
+
+if (somePieces > 0) {
+  let total = unitPrice * somePieces
+  console.log(`Total: ${total}`)
+}
+
+// console.log(total) // Total is not defined (Uncaught Error)
+
+let myUserAge = 24
+let isMale = false
+let points = 900
+let cartValue = 299
+let shippingCost = 9.99
+
+if (myUserAge > 18) {
+  if (cartValue >= 300 || points >= 500) {
+    shippingCost = 0
+  } 
+}
+
+console.log(shippingCost)
+
+// ! Another simple way!
+
+if (myUserAge > 18 && (cartValue >= 300 || points >= 500)) {
+  shippingCost = 0
+}
+
+console.log(shippingCost)

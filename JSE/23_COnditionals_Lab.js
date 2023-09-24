838d4d99ed1a973f50d1a526d9a53e6fc46470cc
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-23 20:38:54 -0600

    JSE: Exercise 3 completed

--- JSE/23_COnditionals_Lab.js
+++ JSE/23_COnditionals_Lab.js
@@ -14,3 +14,31 @@ let mySecondNumber = prompt('Input a number...')
 
 let result = (mySecondNumber > 90 && mySecondNumber < 110) ? 
 alert('Bingo!') : alert('Miss!')
+
+// * Exercise 3
+
+let firstNumber = Number(prompt('First number: '))
+let secondNumber = Number(prompt('Second number: '))
+let selectOperation = prompt('Select the operation:\n+\n-\n*\n/')
+let myResult
+
+switch(selectOperation) {
+  case '+':
+    myResult = firstNumber + secondNumber
+    alert(`${firstNumber} + ${secondNumber} = ${myResult}`)
+    break
+  case '-':
+    myResult = firstNumber - secondNumber
+    alert(`${firstNumber} - ${secondNumber} = ${myResult}`)
+    break
+  case '*':
+    myResult = firstNumber * secondNumber
+    alert(`${firstNumber} * ${secondNumber} = ${myResult}`)
+    break
+  case '/':
+    myResult = firstNumber / secondNumber
+    alert(`${firstNumber} / ${secondNumber} = ${myResult}`)
+    break
+  default:
+    alert('Please enter a valid value!')
+}

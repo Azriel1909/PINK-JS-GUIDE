cc663c1909562717f184e16fd67ada642d134820
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-23 20:30:48 -0600

    JSE: Exercise 2 completed

--- JSE/23_COnditionals_Lab.js
+++ JSE/23_COnditionals_Lab.js
@@ -7,3 +7,10 @@ if(myNumber > 90 && myNumber < 110) {
 } else {
   alert('Miss!')
 }
+
+// * Exercise 2
+
+let mySecondNumber = prompt('Input a number...')
+
+let result = (mySecondNumber > 90 && mySecondNumber < 110) ? 
+alert('Bingo!') : alert('Miss!')

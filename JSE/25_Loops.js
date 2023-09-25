0b1b5a63875c65c1292756fd78e6bcf1ebf4a523
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-09-24 20:04:26 -0600

    JSE: While Loop

---
+++ JSE/25_Loops.js
@@ -0,0 +1,11 @@
+// + Loops: Repeat any fragment of the code as many times as we want.
+
+// * Conditional Statements: They are able to change code behavior, allowing us to decide during the program execution whether a certain piece of code is to be performed or not.
+
+// > While Loop: Iteration
+
+let n = 0
+while (n < 100) {
+  console.log(n)
+  n+=10
+}

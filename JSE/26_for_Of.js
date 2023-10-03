8fbf04e48b302fc8a01ec6c9d5aa09c2848194fe
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-02 21:11:23 -0600

    JSE: For ... Of

--- JSE/26_for_Of.js
+++ JSE/26_for_Of.js
@@ -5,8 +5,16 @@
 let mySuperCoolValues = [1,2,3,4,5,6,7,8,9,10]
 let mySum = 0
 
+// > Simple Version using For Loop
+
 for (let i = 0; i < mySuperCoolValues.length; i++) {
   mySum += mySuperCoolValues[i]
+}
+
+console.log(`Total: ${mySum}`)
+
+for (let number of mySuperCoolValues) {
+  mySum += number
 }
 
 console.log(`Total: ${mySum}`)

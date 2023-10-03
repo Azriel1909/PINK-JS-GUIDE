42652f846c6dc4b3fa417cd86e924c01596f0a3a
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-02 23:15:39 -0600

    JSE: Exercise 2 completed

--- JSE/27_Loop_Practice.js
+++ JSE/27_Loop_Practice.js
@@ -13,3 +13,15 @@ while (j >= 10) {
   console.log(j)
   j-=10
 }
+
+console.log('----------------')
+
+let max = prompt('Enter the maximum value')
+let min = prompt('Enter the minimum value')
+
+if (max > min) {
+  while(max>=min){
+    console.log(max)
+    max-=10
+  }
+}

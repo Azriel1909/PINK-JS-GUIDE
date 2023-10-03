b05ca3a283f05869be1b4f24397e81913ebc2404
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-02 21:09:21 -0600

    JSE: For ... Of

---
+++ JSE/26_for_Of.js
@@ -0,0 +1,12 @@
+// - In addition to the regular for loop there are two specific versions
+
+// * For ... Of is dedicated for use with arrays: Here we do not specify the any conditions or number of iterations, as it is performed exactly as many times as there are elements in the dedicated array.
+
+let mySuperCoolValues = [1,2,3,4,5,6,7,8,9,10]
+let mySum = 0
+
+for (let i = 0; i < mySuperCoolValues.length; i++) {
+  mySum += mySuperCoolValues[i]
+}
+
+console.log(`Total: ${mySum}`)

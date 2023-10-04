d2b57f260a8ce649ea0907e56d25d4197a41cbbc
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-04 00:49:53 -0600

    JSE: Exercise 6 Completed

--- JSE/29_Lab_Calculator.js
+++ JSE/29_Lab_Calculator.js
@@ -5,7 +5,8 @@ while (!exitCalculator) {
   let firstNumber = Number(prompt('First number... '))
   let secondNumber = Number(prompt('Second number... '))
   let selectOperation = prompt('Select the operation:\n+\n-\n*\n/\nOr select Q to exit.')
-  if (firstNumber !== null && firstNumber !== null && selectOperation !== null) {
+
+  if (firstNumber !== null || firstNumber !== null || selectOperation !== null) {
     switch(selectOperation) {
       case '+':
         myResult = firstNumber + secondNumber
@@ -23,11 +24,12 @@ while (!exitCalculator) {
         myResult = firstNumber / secondNumber
         alert(`${firstNumber} / ${secondNumber} = ${myResult}`)
         break
+      case 'Q':
+        exitCalculator = true
+        break
       default:
         alert('Please enter a valid value!')
     }
-  } else if (selectOperation === 'Q') {
-    exitCalculator = true
   }
 }
 

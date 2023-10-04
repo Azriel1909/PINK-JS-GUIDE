3bd241b09f09770651a31a8146713eac9d1ca583
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-04 00:29:00 -0600

    JSE: Exercise 4 Completed

---
+++ JSE/28_Movie_List.js
@@ -0,0 +1,37 @@
+let movies = []
+let noMoreMovies = false
+
+while (!noMoreMovies) {
+  let movieName = prompt('Enter Movie Name.')
+  let movieRating = prompt('Enter Movie Rating.')
+
+  Number(movieRating)
+  
+  if (movieName !== null && movieRating !== null) {
+    movies.push({
+      title: movieName,
+      imdb: movieRating 
+    })
+  } else {
+    noMoreMovies = true
+  }
+}
+
+// ! TESTING
+// for (let movie of movies) {
+//   console.log(`Movie Title: ${movie.title}\nMovie Rating: ${movie.imdb}`)
+// }
+
+console.log('Movies that has been rating less than 7:')
+for (let movie of movies) {
+  if (movie.imdb < 7) {
+    console.log(`Movie Title: ${movie.title},\nMovie Rating: ${movie.imdb}`)
+  }
+}
+
+console.log('Movies that has been rating greater or equal to 7:')
+for (let movie of movies) {
+  if (movie.imdb >= 7) {
+    console.log(`Movie Title: ${movie.title},\nMovie Rating: ${movie.imdb}`)
+  }
+}

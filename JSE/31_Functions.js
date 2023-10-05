4db636ad6dccc9c5e25acce96ae166cafcee4d9e
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-05 00:43:25 -0600

    JSE: Function Declaration

--- JSE/31_Functions.js
+++ JSE/31_Functions.js
@@ -11,3 +11,17 @@
 function mySuperCoolFunction() {
   console.log('My name Ximena!')
 }
+
+// > Example
+
+let temperatures
+let sum
+let meanTemp
+
+function getMeanTemp() {
+  sum = 0
+  for(let i = 0; i < temperatures.length; i++) {
+    sum += temperatures[i]
+  }
+  meanTemp = (sum/temperatures.length)
+}

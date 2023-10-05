f9b286709c3717b90e5b4086c8df87088c3d7f12
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-05 00:40:56 -0600

    JSE: Function Declaration

---
+++ JSE/31_Functions.js
@@ -0,0 +1,13 @@
+// > Function: It is separate piece of code, constituting a certain closed logical whole, intended perform a specific task. 
+
+// ? Why do we use functions? 1. To divide the code into some logical independent parts; 2. If you repeat the sequence of instructions in the program in many places, you can close this sequence in a function; 3. Reducing amount of code. 
+
+// - Modularity increases the readability of code
+
+// > Function Declaration
+
+// * Function Statement: it starts with the function keyword followed by the function name, there are parentheses that can optionally have function parameters. After the parentheses comes the function body, which is made form any number of statements (a code block).
+
+function mySuperCoolFunction() {
+  console.log('My name Ximena!')
+}

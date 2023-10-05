bc6b04da3c621f38d5f3b440742739bdd1c5ebb8
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-05 00:13:10 -0600

    JSE: Challenge Display All Elements

--- JSE/30_.js
+++ JSE/30_.js
@@ -16,27 +16,39 @@ let contacts = [
   }
 ]
 
-let selectOption = prompt('Select an option:\nShow the first contact -> A\nShow the last contact -> B\nAdd a new contact -> C')
+let exitContactList = false
 
-switch(selectOption) {
-  case 'A':
-    alert(`Name: ${contacts[0].name}\nPhone: ${contacts[0].phone}\nEmail: ${contacts[0].email}`)
-    break
-  case 'B':
-    let lastContact = contacts[contacts.length - 1]
-    alert(`Name: ${lastContact.name}\nPhone: ${lastContact.phone}\nEmail: ${lastContact.email}`)
-    break
-  case 'C':
-    let enterName = prompt('Enter name:')
-    let enterPhone = prompt('Enter phone:')
-    let enterEmail = prompt('Enter email:')
-    contacts.push({
-      name: enterName,
-      phone: enterPhone,
-      email: enterEmail
-    })
-    alert(`Name: ${enterName}\nPhone: ${enterPhone}\nEmail: ${enterEmail}`)
-    break
-  default:
-    alert('Please enter a valid value!')
+while (!exitContactList) {
+  
+  let selectOption = prompt('Select an option:\nShow the first contact -> A\nShow the last contact -> B\nShow All contacts -> C\nAdd a new contact -> D\nExit -> E')
+
+  switch(selectOption) {
+    case 'A':
+      alert(`Name: ${contacts[0].name}\nPhone: ${contacts[0].phone}\nEmail: ${contacts[0].email}`)
+      break
+    case 'B':
+      let lastContact = contacts[contacts.length - 1]
+      alert(`Name: ${lastContact.name}\nPhone: ${lastContact.phone}\nEmail: ${lastContact.email}`)
+      break
+    case 'C':
+      for (let contact of contacts){
+        alert(`Name: ${contact.name}\nPhone: ${contact.phone}\nEmail: ${contact.email}`)
+      }
+    case 'D':
+      let enterName = prompt('Enter name:')
+      let enterPhone = prompt('Enter phone:')
+      let enterEmail = prompt('Enter email:')
+      contacts.push({
+        name: enterName,
+        phone: enterPhone,
+        email: enterEmail
+      })
+      alert(`Name: ${enterName}\nPhone: ${enterPhone}\nEmail: ${enterEmail}`)
+      break
+    case 'E':
+      exitContactList = true
+      break
+    default:
+      alert('Please enter a valid value!')
+  }
 }

839da6784419326e9fbcb90a8301ab8c2f5dabe5
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-06 22:15:09 -0600

    JSE: Parameters and Arguments

--- JSE/34_Functions.js
+++ JSE/34_Functions.js
@@ -8,3 +8,27 @@ function addValues(num1, num2){ // ! Parameters
 
 let result = addValues(10,9) // ! Arguments
 console.log(`Result: ${result}`)
+
+function getElement(elements, index){
+  return elements[index]
+}
+
+let names = ['Ximena', 'Ace', 'Volkgan', 'Asya']
+let name = getElement(names, 2)
+console.log(name)
+
+function getMeanTemp(temperatures) {
+  let sum = 0
+  for (let i = 0; i < temperatures.length; i++) {
+    sum += temperatures[i]
+  }
+  return sum / temperatures.length
+}
+
+let day1 = [12, 12, 11, 11, 10, 9, 9, 10, 12, 13, 15, 18, 21, 24, 24, 23, 25, 25, 23, 21, 20, 19, 17, 16]
+
+console.log(`Day 1 Mean: ${getMeanTemp(day1)}`)
+
+let day2 = [17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24, 21, 19, 18, 17, 16]
+
+console.log(`Day 2 Mean: ${getMeanTemp(day2)}`)

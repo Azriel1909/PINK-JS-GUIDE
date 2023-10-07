9b3bf12722bccfebfbb270925cf2aa3be958b9be
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-06 21:57:17 -0600

    JSE: Return Statement

--- JSE/32_Functions.js
+++ JSE/32_Functions.js
@@ -20,3 +20,29 @@ console.log(`Mena: ${meanTemp}`)
 temperatures = [17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24, 21, 19, 18, 17, 16];
 getMeanTemp()
 console.log(`Mena: ${meanTemp}`)
+
+// > The return Statement: Functions that have been called execute a sequence of instructions contained in their body. The result of this execution may be a certain value that we may want to store in some variable.
+
+
+// * 1. Return, causes the function to end exactly where this word occurs, even if there are further instructions.
+
+// * 2. Return, allow us to return a given value from inside the function to the place where it was called. 
+
+console.log('----------------------------')
+
+function showAMessage(){
+  console.log('My message 1')
+  return
+  console.log('My message 2')
+}
+
+showAMessage()
+
+// * Returns only shows the content of the 1st console.log, returns, interrupt the function.
+console.log('----------------------------')
+
+function getTrue(){
+  return true;
+}
+let getSignal = getTrue()
+console.log(`Signal - ${getSignal}`)

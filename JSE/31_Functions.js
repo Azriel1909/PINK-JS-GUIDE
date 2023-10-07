4a05821ee22e6607818c457a61fed04b1e1ca385
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-06 21:41:02 -0600

    JSE: Called Function

--- JSE/31_Functions.js
+++ JSE/31_Functions.js
@@ -25,3 +25,15 @@ function getMeanTemp() {
   }
   meanTemp = (sum/temperatures.length)
 }
+
+// > Calling Functions: To call a function we write the name and follow with parentheses.
+
+temperatures = [12, 12, 11, 11, 10, 9, 9, 10, 12, 13, 15, 18, 21, 24, 24, 23, 25, 25, 23, 21, 20, 19, 17, 16]
+getMeanTemp()
+console.log(`Mena: ${meanTemp}`)
+
+temperatures = [17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24, 21, 19, 18, 17, 16];
+getMeanTemp()
+console.log(`Mena: ${meanTemp}`)
+
+// - Usually, functions are declared before they are called, mostly at the beginning of the code (it is a good practice, which increases the readability).

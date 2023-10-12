ef41a2ddf5361ae8ea8b0bea14649fc32a506621
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-11 21:02:30 -0600

    JSE: Arrow Functions Exercise 2 Completed

--- JSE/38_Practice_.js
+++ JSE/38_Practice_.js
@@ -30,3 +30,15 @@ let myCompareNumbersFunction = (a,b) => {
 
 let mySortedNumbers = numbers.sort(myCompareNumbersFunction)
 console.log(mySortedNumbers)
+
+console.log('----------------- Descending Order')
+
+let descendingOrder = (a,b) => {
+  let myValue = 0
+  return a < b ? myValue = 1:
+         a > b ? myValue = -1:
+         myValue
+}
+
+let descendingOrderNumbers = numbers.sort(descendingOrder)
+console.log(descendingOrderNumbers)

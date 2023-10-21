d58d20ba15a76d42954974a0cf540a14b55b9439
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-20 21:33:07 -0600

    JSE: Errors and Exceptions

---
+++ JSE/41_Erros.js
@@ -0,0 +1,13 @@
+// Errors and Exceptions
+
+// * Edsger W. Dijkstra: "If debugging is the process of removing software bugs, then programming must be the process of putting them in"
+
+// - Grammar = Syntax of programming languages: Set of rules defining the structure of the instructions
+
+// > Natural Languages and Communication Errors: Syntax,Errors, Semantic Errors and Logical Errors.
+
+// > Errors and exceptions in JavaScript
+
+let multiply = (a, b) => a + b
+let result = multiply(10,9)
+console.log(result)

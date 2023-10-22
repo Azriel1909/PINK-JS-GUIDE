9d8adcab4f7faa66e39e3bffe007d0ec13504db4
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-21 21:47:47 -0600

    JSE: Errors Types

--- JSE/41_Erros.js
+++ JSE/41_Erros.js
@@ -8,6 +8,14 @@
 
 // > Errors and exceptions in JavaScript
 
-let multiply = (a, b) => a + b
+// ! let multiply = (a b) => a + b SyntaxError 
+// ! let multipl = (a b) => a + b Uncaught Error: multipl is not defined
+// ! let multiply = (a b) => a + b Logical Error
+
+let multiply = (a, b) => a * b
 let result = multiply(10,9)
 console.log(result)
+
+// * When JavaScript detects syntactic or semantic errors, it generates and throws specific objects containing information about the encountered error.
+
+// > Run Time Errors

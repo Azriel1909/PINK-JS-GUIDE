cd09cd9f2c9273d32ec2b15bb372d61fd70dc2af
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-22 19:27:49 -0600

    JSE: Syntaz Error

---
+++ JSE/42_Exceptions.js
@@ -0,0 +1,9 @@
+// > Basic types of errors
+
+// ! syntaxError: It appears when there are typos in the keywords, unmatching parentheses or brackets.
+
+// iff (true) {
+//   console.log('true')
+// }
+
+// !

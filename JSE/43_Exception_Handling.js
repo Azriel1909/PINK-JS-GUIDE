0977b85bc192aadf262fa2b12f2f704803cbd3e8
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-23 21:21:55 -0600

    JSE: Finally Statement

--- JSE/43_Exception_Handling.js
+++ JSE/43_Exception_Handling.js
@@ -17,3 +17,16 @@ try {
 console.log(a)
 
 // ? It is important to know that any variable that gets declared using the let keyword inside a try block is not accessible in the catch block.
+
+// > Finally Statement: It can be used with or without the catch block , and it is always executed after try and catch blocks.
+
+let b = 10
+try {
+  b = 5
+  console.log(`Try block: ${b}`)
+} finally {
+  console.log(`Finally block: ${b}`)
+}
+console.log(`Outside: ${b}`)
+
+// - The new value has been stored, without calling and error, in variable a. Then the content of the finally block is executed on the console. The program continues after leaving the finally block, displaying our variable again.

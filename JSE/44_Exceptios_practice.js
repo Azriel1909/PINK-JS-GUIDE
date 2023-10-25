748a7280357e0173669896afcfbf625b0518e4a8
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-24 21:34:21 -0600

    Exceptions: Exercise 2 completed

--- JSE/44_Exceptios_practice.js
+++ JSE/44_Exceptios_practice.js
@@ -1,14 +1,31 @@
-function div(a,b){
+// * Exercise 1
+
+console.log('--------------')
+
+function myDiv(a,b){
   if (b === 0){
     throw new RangeError('Dividing by zero is infinity.')
   }
   return a/b
 }
-console.log(div(9,3))
+console.log(myDiv(9,3))
 
 try {
-  console.log(div(9,0))
+  console.log(myDiv(9,0))
 } catch (error) {
   console.log(`> ${error}`)
 }
 
+// * Exercise 2
+
+console.log('--------------')
+
+let numbers = [10, 40, 0, 20, 50];
+for (number of numbers) {
+  let maxValue = 1000
+  try {
+    console.log(myDiv(maxValue,number))
+  } catch (error) {
+    console.log(`> ${error}`)
+  }
+}

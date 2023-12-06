71de3b6fce9691921701e446146da91827c9e0c5
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-12-05 21:38:38 -0600

    JSE: Introduction to Objects

---
+++ JSE/49_Object.js
@@ -0,0 +1,9 @@
+// > Primitive Data types
+
+// * Number, Boolean, Strings
+
+// > Typeof command
+
+console.log(typeof(2.5))
+console.log(typeof('PinkHat Owner, Ximena Toledo'))
+console.log(typeof(true))

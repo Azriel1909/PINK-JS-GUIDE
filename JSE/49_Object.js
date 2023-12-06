14aa95ecce7aaad9a87e4a25398be9381f734e28
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-12-05 21:49:17 -0600

    JSE: Introduction to Objects

--- JSE/49_Object.js
+++ JSE/49_Object.js
@@ -1,4 +1,4 @@
-// > Primitive Data types
+// > Primitive Data types: They are not complex, there is always one specific, simple value in a variable.
 
 // * Number, Boolean, Strings
 
@@ -13,3 +13,22 @@ console.log(typeof(true))
 let myGrade = 9.0
 myGrade = myGrade/2
 console.log(myGrade)
+console.log(typeof(myGrade))
+
+// ! Note: If we did not explicitly assigned any value to a declared variable, it contains a undefined value by default. The Undefined value is also treated as one of the primitive data type.
+
+// * In JavaScript everything, except a primitive, is an object.
+
+// > Array/Object as a complex type
+
+// * Complex types, such as arrays or objects are Data Collections; An ordered set of different values can be placed in one variable.
+
+// - Element: Individual values placed in a single variable. They are referred to by a number specifying their position in the collection (an index)
+
+let myArray = [10, false, 60, 'Ximena', 34.7, 10]
+myArray[5] = myArray[5] * 4
+console.log(myArray[0])
+console.log(myArray[2])
+console.log(myArray[5])
+console.log(typeof(myArray))
+

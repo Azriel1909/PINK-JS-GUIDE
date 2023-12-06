560e5ba07fb190bef69d298894b04558eb138d1d
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-12-05 21:40:23 -0600

    JSE: Introduction to Objects

--- JSE/49_Object.js
+++ JSE/49_Object.js
@@ -7,3 +7,9 @@
 console.log(typeof(2.5))
 console.log(typeof('PinkHat Owner, Ximena Toledo'))
 console.log(typeof(true))
+
+// * Values can be replaced in variables
+
+let myGrade = 9.0
+myGrade = myGrade/2
+console.log(myGrade)

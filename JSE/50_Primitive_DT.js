b73c8d0b7584e9d4cf00c2637c8eca065138b06d
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-12-08 21:08:37 -0600

    JSE: Primitive Data Types

--- JSE/50_Primitive_DT.js
+++ JSE/50_Primitive_DT.js
@@ -3,3 +3,7 @@
 console.log(typeof(9.9))
 console.log(typeof('Ximena Toledo'))
 console.log(typeof(false))
+
+let myFloat = 9.5
+myFloat = myFloat/2
+console.log(`Result: ${myFloat}\nData type: ${typeof(myFloat)}`)

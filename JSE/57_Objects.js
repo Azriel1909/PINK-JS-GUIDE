4c294378a766b1fc1f075c569175bdd6266cc25a
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-12-27 21:43:56 -0600

    JSE: Factory

--- JSE/57_Objects.js
+++ JSE/57_Objects.js
@@ -4,11 +4,11 @@
 
 // - Idea: We create a function that will return a new object of our defined types every time it is called.
 
-let myPoint = function(x,y) {
-  let obj = {}
-  obj.x = x
-  obj.y = y
-  return obj
+let myPoint = function(x,y) { // ! This is the factory
+  let obj = {} // ! We create an empty object
+  obj.x = x // ! Adding the properties
+  obj.y = y // ! Adding the properties
+  return obj // ! Create the object
 }
 
 let myPointA = myPoint(1,1)
@@ -20,3 +20,25 @@ console.log(`A (${myPointA.x},${myPointA.y})`)
 console.log(`B (${myPointB.x},${myPointB.y})`)
 console.log(`C (${myPointC.x},${myPointC.y})`)
 console.log(`D (${myPointD.x},${myPointD.y})`)
+
+// * Let's improve it!
+
+let trackPoint = function(x,y) {
+  return {
+    x:x,
+    y:y
+  }
+}
+
+// * Let's make it simpler
+
+// - We can insert the arguments into the object without separating them into key and value
+
+let point = function(x,y){
+  return{
+    x,
+    y
+  }
+}
+
+// * Let's make it shorter

5125207925b4d8d9662a80e4f0c954644142071b
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-12-28 20:53:32 -0600

    JSE: Normal Notation

--- JSE/58_Factory.js
+++ JSE/58_Factory.js
@@ -9,3 +9,14 @@ let createdColoredPoint = function(x,y,color) {
     getColor() { return _color}
   }
 }
+
+let coloredPoint1 = createdColoredPoint(1, 1, 'pink')
+let coloredPoint2 = createdColoredPoint(1, 2, 'white')
+let coloredPoint3 = createdColoredPoint(1, 3, 'black')
+let coloredPoint4 = createdColoredPoint(1, 4, 'blue')
+
+console.log(`Point: (${coloredPoint1.x},${coloredPoint1.y})\tColor: ${coloredPoint1.getColor()}`)
+console.log(`Point: (${coloredPoint2.x},${coloredPoint2.y})\tColor: ${coloredPoint2.getColor()}`)
+console.log(`Point: (${coloredPoint3.x},${coloredPoint3.y})\tColor: ${coloredPoint3.getColor()}`)
+console.log(`Point: (${coloredPoint4.x},${coloredPoint4.y})\tColor: ${coloredPoint4.getColor()}`)
+

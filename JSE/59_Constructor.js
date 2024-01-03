033fec7792eb687d22be0a77ba603c4341b3a548
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2024-01-02 21:53:36 -0600

    Cv updated

--- JSE/59_Constructor.js
+++ JSE/59_Constructor.js
@@ -4,7 +4,9 @@
 
 // - A constructor, like a factory, is a function that creates and returns an object. It differs from the factory in several respects, 1st, it requieres the use of the keyword 'new' when creating a new object.
 
-let myColoredPoint = function (x, y, color) {
+// * Constructor names begin with capital letters 
+
+let MyColoredPoint = function (x, y, color) {
   let _info = '... Object under construction'
   let _color = color
   console.log(_info)
@@ -13,10 +15,10 @@ let myColoredPoint = function (x, y, color) {
   this.getColor = function () { return _color }
 }
 
-let coloredPoint1 = new myColoredPoint(1, 1, 'pink')
-let coloredPoint2 = new myColoredPoint(1, 2, 'red')
-let coloredPoint3 = new myColoredPoint(1, 3, 'white')
-let coloredPoint4 = new myColoredPoint(1, 4, 'black')
+let coloredPoint1 = new MyColoredPoint(1, 1, 'pink')
+let coloredPoint2 = new MyColoredPoint(1, 2, 'red')
+let coloredPoint3 = new MyColoredPoint(1, 3, 'white')
+let coloredPoint4 = new MyColoredPoint(1, 4, 'black')
 
 console.log(`Point: (${coloredPoint1.x}, ${coloredPoint1.y})\tColor: ${coloredPoint1.getColor()}`)
 console.log(`Point: (${coloredPoint2.x}, ${coloredPoint2.y})\tColor: ${coloredPoint2.getColor()}`)
@@ -27,3 +29,9 @@ console.log(coloredPoint1._color)
 console.log(coloredPoint2._color)
 console.log(coloredPoint3._color)
 console.log(coloredPoint4._color)
+
+// ? Differences
+
+// - No return keyword, the constructor returns automatically the implicitly created object.
+
+// * Because the constructor is a function we can also use the closure here.

648ce232b22a2e6a10553788461fe736b3d459df
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2024-01-04 20:51:39 -0600

    JSE: Prototypes

--- JSE/61_Prororypes.js
+++ JSE/61_Prororypes.js
@@ -9,9 +9,17 @@ coloredPoint._proto_ = point
 
 // - point has also its own prototype, which is created by default on the basis of the object constructor
 
-// * Trying to refer the property of the coloredPoint Object
+// * Trying to refer the property of the coloredPoint Object.
 
 console.log(`property names: ${Object.getOwnPropertyNames(coloredPoint)}`)
 console.log(`color: ${coloredPoint.color}`)
 console.log(`x: ${coloredPoint.x}`)
 
+// ! JavaScript does not find the field name x in the coloredPoint object and looks for it in the prototype.
+
+// * Trying to change the value of the property we inherited from the prototype.
+
+coloredPoint.x = 900 // ! New Property
+console.log(`x: ${coloredPoint.x}`)
+console.log(`x: ${point.x}`)
+console.log(`property names: ${Object.getOwnPropertyNames(coloredPoint)}`)

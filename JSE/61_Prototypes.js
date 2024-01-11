a79cc6e5f77831087c45607a5ca38e13ccf116d4
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2024-01-10 21:57:53 -0600

    JSE: COnstructor

--- JSE/61_Prototypes.js
+++ JSE/61_Prototypes.js
@@ -101,6 +101,16 @@ MyIncredibleCircle.prototype = myFigure
 let circle1 = new MyIncredibleCircle({x:10,y:10}, 10)
 let circle2 = new MyIncredibleCircle({x:20,y:20}, 20)
 
+let Triangle = function(v1,v2,v3){
+  this.type = 'Triangle'
+  this.vertices = [v1,v2,v3]
+}
+
+Triangle.prototype = myFigure
+let triangle1 = new Triangle({x:0, y:0}, {x:50, y:50}, {x:10, y:100});
+console.log(circle1.getType());
+console.log(triangle1.getType());
+
 
 
 

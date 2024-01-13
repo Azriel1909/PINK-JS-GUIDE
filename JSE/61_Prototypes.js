cae1fff19dd02038a4b1e0b004a58cdb3fe85f45
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2024-01-12 22:45:18 -0600

    JSE: Prototypes

--- JSE/61_Prototypes.js
+++ JSE/61_Prototypes.js
@@ -109,4 +109,4 @@ let Triangle = function(v1,v2,v3){
 Triangle.prototype = myFigure
 let triangle1 = new Triangle({x:0, y:0}, {x:50, y:50}, {x:10, y:100});
 console.log(circle1.getType());
-console.log(triangle1.getType());
+console.log(`TYpe: ${triangle1.getType()}`);
